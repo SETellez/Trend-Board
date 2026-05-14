8472eda69a3dce81c46800fed4d0fbc5a46b074b
Trend Board Dev <dev@trend-board.com>
2026-05-14 17:24:20 -0500

    fix: actualizar CORS para permitir origen de Vercel #HU-2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:4000' });
+  app.enableCors({
+    origin: ['http://localhost:4000', 'https://trend-board-two.vercel.app'],
+  });
   await app.listen(3000);
 }
 bootstrap();
